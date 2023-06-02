8cc7dc60799cb6af59d8d0b2f31b52b9485ac2b6
tablit <tata.angelo@gmail.com>
2023-06-02 00:56:26 -0300

    Add terraform state s3 config

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -37,3 +37,12 @@ resource "aws_s3_object" "job_spark" {
 provider "aws" {
   region = "us-west-2"
 }
+
+#Centraliza o arquivo de controle de estado do terraform
+terraform {
+  backend "s3" {
+    bucket = "terraform-state-igti-talita"
+    key = "state/igti/edc/mod1/terraform.tfstate"
+    region = "us-west-2"
+  }
+}
